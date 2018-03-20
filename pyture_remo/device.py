d34031c7c357ff44cea629d0b2b728bd3d859974
Suzuka Asagiri <suzutan0s2@suzutan.jp>
2018-03-20 22:12:50 +0900

    :recycle: Fix extend

--- pyture_remo/device.py
+++ pyture_remo/device.py
@@ -1,27 +1,23 @@
-class Device:
+from .base import Base
 
-    def __str__(self) -> str:
-        return "\n".join([
-            f"{x.ljust(20)}= {getattr(self, x)}" for x in [
-                y for y in dir(self) if not y.startswith("__") and not y.endswith("__")
-            ]
-        ])
 
-    def __init__(self, device_data: dict):
-        self.name: str = device_data["name"]
-        self.id: str = device_data["id"]
-        self.created_at: str = device_data["created_at"]
-        self.updated_at: str = device_data["updated_at"]
-        self.firmware_version: str = device_data["firmware_version"]
-        self.temperature_offset: str = device_data["temperature_offset"]
-        self.humidity_offset: str = device_data["humidity_offset"]
-        self.firmware_version: str = device_data["firmware_version"]
-        self.firmware_version: str = device_data["firmware_version"]
+class Device(Base):
+
+    def __init__(self, data: dict):
+        self.name: str = data["name"]
+        self.id: str = data["id"]
+        self.created_at: str = data["created_at"]
+        self.updated_at: str = data["updated_at"]
+        self.firmware_version: str = data["firmware_version"]
+        self.temperature_offset: str = data["temperature_offset"]
+        self.humidity_offset: str = data["humidity_offset"]
+        self.firmware_version: str = data["firmware_version"]
+        self.firmware_version: str = data["firmware_version"]
         self.humidity: dict = {
-            "value": device_data["newest_events"]["hu"]["val"],
-            "created_at": device_data["newest_events"]["hu"]["created_at"],
+            "value": data["newest_events"]["hu"]["val"],
+            "created_at": data["newest_events"]["hu"]["created_at"],
         }
         self.temperature: dict = {
-            "value": device_data["newest_events"]["te"]["val"],
-            "created_at": device_data["newest_events"]["te"]["created_at"],
+            "value": data["newest_events"]["te"]["val"],
+            "created_at": data["newest_events"]["te"]["created_at"],
         }
