6ff7d4628c036d7e14eca5d85f05c0d723469802
Suzuka Asagiri <suzutan0s2@suzutan.jp>
2018-03-20 21:52:05 +0900

    :new: Add Signal class

--- pyture_remo/signal.py
+++ pyture_remo/signal.py
@@ -1,2 +1,17 @@
-class Signal:
-    pass
+from .api import Api
+from .base import Base
+
+
+class Signal(Base):
+
+    def __str__(self):
+        return self.name
+
+    def __init__(self, id: str, name: str = None, image: str = None):
+        self.id: str = id
+        self.name: str = name
+        self.image: str = image
+        self.api: Api = Api.instance()
+
+    def send(self) -> dict:
+        return self.api.post(path=f"/signals/{self.id}/send")
