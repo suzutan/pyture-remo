d32dc68889811e12de7aa0179dcf6e0ca04e2da9
Suzuka Asagiri <admin@suzutan.jp>
2020-02-02 16:37:25 +0900

    Class importを動的に設定

--- pyture_remo/__init__.py
+++ pyture_remo/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from .appliance import Appliance
-from .device import Device
-from .signal import Signal
-from .remo import Remo
-from .user import User
+import os
+import glob
+
+__all__ = [
+    os.path.split(os.path.splitext(file)[0])[1]
+    for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9]*.py'))
+]
