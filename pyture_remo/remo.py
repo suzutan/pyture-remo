8019b43de97b4c3e4bb177bad84812c9d458cbf3
Suzuka Asagiri <admin@suzutan.jp>
2020-02-02 15:51:08 +0900

    remo.py のapi request回数の削減

--- pyture_remo/remo.py
+++ pyture_remo/remo.py
@@ -2,36 +2,45 @@
 
 from .device import Device
 from .api import Api
-from typing import List
+from typing import List, NoReturn
 from .appliance import Appliance
 
 
 class Remo:
 
     def __init__(self, token: str):
-        self.id: str = None
-        self.nickname: str = None
-        self.api: Api = Api()
-        self.api.init(token=token)
+        self._id: str = None
+        self._nickname: str = None
+        self._devices:  List[Device] = []
+        self._appliances:  List[Appliance] = []
+        self._api: Api = Api()
+        self._api.init(token=token)
+        self.sync()
+
+    def sync(self) -> NoReturn:
+        self._devices = \
+            [Device(data=device) for device in self._api.get(path="/1/devices")]
+        self._appliances = \
+            [Appliance(data=appliance) for appliance in self._api.get(path="/1/appliances")]
 
     def devices(self) -> List[Device]:
-        return [Device(data=device) for device in self.api.get(path="/1/devices")]
+        return self._devices
 
     def device(self, name: str) -> Device:
-        result = list(filter(lambda x: name == x["name"], self.api.get(path="/1/devices")))
+        result = list(filter(lambda x: name == x.name, self._devices))
 
         if not len(result):
             raise ValueError(name)
 
-        return Device(data=result[0])
+        return result[0]
 
     def appliances(self) -> List[Appliance]:
-        return [Appliance(data=appliance) for appliance in self.api.get(path="/1/appliances")]
+        return self._appliances
 
     def appliance(self, name: str) -> Appliance:
-        result = list(filter(lambda x: name == x["nickname"], self.api.get(path="/1/appliances")))
+        result = list(filter(lambda x: name == x.nickname,self._appliances))
 
         if not len(result):
             raise ValueError(name)
 
-        return Appliance(data=result[0])
+        return result[0]
