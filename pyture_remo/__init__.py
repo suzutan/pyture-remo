f50d234b5cd3a34c4fa3150aa3c184d7bfb0b717
Suzuka Asagiri <admin@suzutan.jp>
2020-02-02 17:09:19 +0900

    test_*.py をimportから除外

--- pyture_remo/__init__.py
+++ pyture_remo/__init__.py
@@ -5,5 +5,6 @@ import glob
 
 __all__ = [
     os.path.split(os.path.splitext(file)[0])[1]
-    for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9]*.py'))
+    for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9]*.py') )
+    if "test_" not in file
 ]
