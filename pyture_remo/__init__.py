99c2cf792e2f1965f49fe936a610da5dadd27912
Suzuka Asagiri <admin@suzutan.jp>
2020-02-06 02:47:13 +0900

    Fix import

--- pyture_remo/__init__.py
+++ pyture_remo/__init__.py
@@ -1,10 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-import glob
-
-__all__ = [
-    os.path.split(os.path.splitext(file)[0])[1]
-    for file in glob.glob(os.path.join(os.path.dirname(__file__), '[a-zA-Z0-9]*.py') )
-    if "_test" not in file
-]
+from .appliance import Appliance
+from .device import Device
+from .remo import Remo
+from .signal import Signal
