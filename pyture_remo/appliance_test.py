d65fb1df40ff68e608d384a3b101b98423647e5d
Suzuka Asagiri <admin@suzutan.jp>
2020-02-06 02:54:47 +0900

    appliance_testを追加

---
+++ pyture_remo/appliance_test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import json
+import unittest
+from pathlib import Path
+
+from .appliance import Appliance
+
+
+class TestAppliance(unittest.TestCase):
+
+    def test_find_signal(self):
+        with (Path(".") / "pyture_remo" / "testdata" / "appliances.json").open("r") as f:
+            appliance_data = json.load(f)
+        appliance: Appliance = Appliance(appliance_data[0])
+
+        self.assertEqual(appliance.signal("全灯").id, "ef0debba-49f1-4b0d-a4b2-0d8ab8dfc4f7")
