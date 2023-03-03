5fa8003ec3407077fde1e8d6df01c336ff74ff6c
Suzuka Asagiri <6679870+suzutan@users.noreply.github.com>
2023-03-03 14:44:45 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyture-remo',
-    version='0.2.2',
+    version='0.2.3',
     description='nature-remo library for Python',
     url='https://github.com/suzutan/pyture-remo',
-    author='Suzuka Asagiri',
-    author_email='admin@suzutan.jp',
+    author='suzuka',
+    author_email='6679870+suzutan@users.noreply.github.com',
     license='MIT',
     keywords='nature-remo',
     packages=find_packages(),
     install_requires=[
-        "requests>=2.22.0"
+        "requests>=2.28.0"
     ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.11',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
     ],
